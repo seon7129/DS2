3f3fec540710f3731aafe662eefc282332ef7a32
seon7129 <seon7129@naver.com>
2022-09-08 12:13:45 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+}Node;
+
+void printList(Node* list)
+{
+    for(Node *curr = list; curr != NULL; curr = curr->next)
+        printf("%d ", curr->data);
+    printf("\n");
+}
+
+void printListReverse(Node* list)
+{
+    Node *curr = list;
+    for(; curr->next != NULL; curr = curr->next);
+    for (; curr != NULL; curr = curr->prev)
+        printf("%d ", curr->data);
+    printf("\n");
+}
+
+Node *creat_node(int value)
+{
+    Node *newNode;
+    newNode = (Node*) malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    return (newNode);
+}
+
+void addList(Node *list, int value)
+{
+    Node* curr = list;
+    //가장 끝까지 간다
+    for (;curr != NULL; curr = curr->next);
+    //만들고 붙인다
+    curr->next = creat_node(value);
+    curr->next->prev = curr;
+}
+
+int main()
+{
+    Node *list = creat_node(10);
+    addList(list, 20);
+    addList(list, 30);
+    addList(list, 40);
+    addList(list, 50);
+
+    printList(list);
+    printListReverse(list);
+
+    return (0);
+}
---
+++ stack.c
@@ -0,0 +1,3 @@
+//
+// Created by JEONG_YUSEON on 2022-09-08.
+//
